2dfc77e547f26185084fca4938bc38a658a38385
sergey <sergey@bolshchikov.net>
2012-03-17 09:12:03 +0200

    splash done

--- index.py
+++ index.py
@@ -6,13 +6,19 @@ import os
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class HomeHandler(webapp2.RequestHandler):
+class IndexHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render())
+
+class EditorHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('editor.html')
+        self.response.out.write(template.render())
         
       
-app = webapp2.WSGIApplication([('/', HomeHandler)], debug = True)    
+app = webapp2.WSGIApplication([('/', IndexHandler),
+                               ('/editor', EditorHandler)], debug = True)    
     
 def main():
     app.run()
