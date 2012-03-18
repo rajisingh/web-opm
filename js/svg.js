3d0f4a7130d70ab7adc1bc9b51f1b3be066a0480
sergey <sergey@bolshchikov.net>
2012-03-18 19:48:16 +0200

    obj drawing done

--- js/svg.js
+++ js/svg.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
-	$(".canvas").svg();
-	
-});
+function init(svg){
+	/*Initial actions to be done
+	 * right after the svg canvas is created*/
+	svg.group("sd");
+}
 
+$(document).ready(function(){
+	$(".canvas").svg({onLoad: init});
+});
+var objId = 0
 function addObject(){
+	objId++;
 	var svg = $(".canvas").svg("get");
-	svg.rect(150, 100, 110, 70, {fill: 'none', stroke: 'limeGreen', strokeWidth: '2'});
+	svg.group($("#sd"), "obj" + objId);
+	svg.rect($("#obj"+objId), 150, 100, 110, 70, {fill: 'none', stroke: 'limeGreen', strokeWidth: '2'});
+	svg.text($("#obj"+objId), 176, 142, "Object "+objId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
 }
