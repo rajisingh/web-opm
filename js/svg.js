4fd5b44553d611394894f3f664a91d169d775cd8
sergey <sergey@bolshchikov.net>
2012-03-26 11:01:15 +0200

    obj dragging enabled

--- js/svg.js
+++ js/svg.js
@@ -1,4 +1,4 @@
-var active_diagram;
+var activeDiagram;
 function randomFromTo(from, to){
 	return Math.floor(Math.random() * (to - from + 1) + from);
 }
@@ -6,7 +6,7 @@ function init(svg){
 	/*Initial actions to be done
 	 * right after the svg canvas is created*/
 	var diagram = svg.group("sd");
-	active_diagram = "sd";
+	activeDiagram = "sd";
 }
 
 $(document).ready(function(){
@@ -17,33 +17,56 @@ var objId = 0
 function addObject(){
 	objId++;
 	var svg = $(".canvas").svg("get");
-	svg.group($("#"+active_diagram), "obj"+objId);
+	var obj = svg.group($("#"+activeDiagram), "obj"+objId, {transform: 'matrix(1 0 0 1 0 0)'});
 	xpos = randomFromTo(90, 1150);
 	ypos = randomFromTo(5, 420);
-	svg.rect($("#obj"+objId), xpos, ypos, 110, 70, {fill: 'none', stroke: 'limeGreen', strokeWidth: '2'});
-	svg.text($("#obj"+objId), xpos+26, ypos+42, "Object "+objId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
+	svg.rect(obj, xpos, ypos, 110, 70, {fill: 'white', stroke: 'limeGreen', strokeWidth: '2'});
+	svg.text(obj, xpos+26, ypos+42, "Object "+objId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
 	
-	//Bind methods of the object
-	$("#obj"+objId).bind({
-		click: function(){
-			//enable drawing markers
-		},
-		mouseover: function(){
-			$(this).css("cursor", "pointer");
-		}
-		mousedown: function(){
-			//enable dragging
-		}
-	})
+	//Binding methods to object
+	$(obj)
+		.mousedown(select)
+		.mousemove(dragging)
+		.mouseup(deselect)
+		.mouseover(pointer);
 }
 
 var procId = 0;
 function addProcess(){
 	procId++;
 	var svg = $(".canvas").svg("get");
-	svg.group($("#"+active_diagram), "proc"+procId);
+	svg.group($("#"+activeDiagram), "proc"+procId);
 	xpos = randomFromTo(90, 1150);
 	ypos = randomFromTo(20, 420);
-	svg.ellipse($("#proc"+procId), xpos, ypos, 60, 40, {fill: 'none', stroke: 'RoyalBlue', strokeWidth: '2'});
+	svg.ellipse($("#proc"+procId), xpos, ypos, 60, 40, {fill: 'white', stroke: 'RoyalBlue', strokeWidth: '2'});
 	svg.text($("#proc"+procId), xpos-33, ypos+6, "Process "+procId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
+}
+
+var selectedElement;
+var currentX = 0;
+var currentY = 0;
+var currentMatrix = 0;
+function select(evt){
+	selectedElement = evt.target;
+	currentX = evt.pageX;
+	currentY = evt.pageY;
+	currentMatrix = $(this).attr('transform').slice(7, -1).split(' ');
+	for (var i=0; i<currentMatrix.length; i++)	{ currentMatrix[i] = parseFloat(currentMatrix[i]); }
+}
+function dragging(evt){
+
+	dx = evt.pageX - currentX;
+	dy = evt.pageY - currentY;
+	currentMatrix[4] += dx;
+	currentMatrix[5] += dy;
+	var newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+	$(this).attr('transform', newMatrix);
+	currentX = evt.pageX;
+	currentY = evt.pageY;
+}
+function deselect(evt){
+	$(this).stop();
+}
+function pointer(){
+	$(this).css('cursor', 'pointer');
 }
