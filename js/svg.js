949bbfcc12b0e1e30b487dae6846ab71fae10a8f
sergey <sergey@bolshchikov.net>
2012-03-26 11:23:19 +0200

    proc dragging enabled

--- js/svg.js
+++ js/svg.js
@@ -33,11 +33,16 @@ var procId = 0;
 function addProcess(){
 	procId++;
 	var svg = $(".canvas").svg("get");
-	svg.group($("#"+activeDiagram), "proc"+procId);
+	var proc = svg.group($("#"+activeDiagram), "proc"+procId, {transform: 'matrix(1 0 0 1 0 0)'});
 	xpos = randomFromTo(90, 1150);
 	ypos = randomFromTo(20, 420);
 	svg.ellipse($("#proc"+procId), xpos, ypos, 60, 40, {fill: 'white', stroke: 'RoyalBlue', strokeWidth: '2'});
 	svg.text($("#proc"+procId), xpos-33, ypos+6, "Process "+procId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
+	
+	//Binding methods to process
+	$(proc)
+		.mousedown(select)
+		.mouseover(pointer);
 }
 
 var selectedElement;
