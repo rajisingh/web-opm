037b8a6c9945780f92e084fe836531b605a3564f
sergey <sergey@bolshchikov.net>
2012-03-27 13:35:20 +0200

    1. key words updated 2. gui classes added

---
+++ js/editor-class.js
@@ -0,0 +1,17 @@
+/*	
+ * 	Web OPM: online case tool for Object-Process Methodology
+ * 	Copyright © 2012 Israel Institute of Technology - Technion
+ * 	The code is licensed under GNU General Public License, v2
+ * 
+ * 	File context description:
+ * 	File contains classes description used for GUI 
+ * */
+
+function selectedThing(id, type){
+	this.id = id
+	this.type = type
+	
+	
+}
+
+function selectedLink(){}
--- js/editor-svg.js
+++ js/editor-svg.js
@@ -1,3 +1,13 @@
+/*	
+ * 	Web OPM: online case tool for Object-Process Methodology
+ * 	Copyright © 2012 Israel Institute of Technology - Technion
+ * 	The code is licensed under GNU General Public License, v2
+ * 
+ * 	File context description:
+ * 	File contains functions for work w/ SVG canvas 
+ * 	via jQuery SVG library  
+ * */
+
 var activeDiagram;
 function randomFromTo(from, to){
 	return Math.floor(Math.random() * (to - from + 1) + from);
