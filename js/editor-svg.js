08b8ba51bc764543443446c4966cbc1fa41260bb
sergey <sergey@bolshchikov.net>
2012-04-08 13:33:52 +0300

    element selecting enabled

--- js/editor-svg.js
+++ js/editor-svg.js
@@ -90,7 +90,7 @@ function addSVGObject() {
 	var obj = document.createElementNS(svgNS, 'g');
 	obj.setAttributeNS(null, 'id', 'obj' + objId);
 	obj.setAttributeNS(null, 'transform', 'matrix(1 0 0 1 0 0)');
-	obj.setAttributeNS(null, 'onmousedown', 'select(evt)');
+	obj.setAttributeNS(null, 'onclick', 'select(evt)');
 	activeDiagram.appendChild(obj);
 	createSVGElement(obj, 'object');
 }
@@ -100,24 +100,41 @@ function addSVGProcess() {
 	var prc = document.createElementNS(svgNS, 'g');
 	prc.setAttributeNS(null, 'id', 'prc' + prcId);
 	prc.setAttributeNS(null, 'transform', 'matrix(1 0 0 1 0 0)');
-	prc.setAttributeNS(null, 'onmousedown', 'select(evt)');
+	prc.setAttributeNS(null, 'onclick', 'select(evt)');
 	activeDiagram.appendChild(prc);
 	createSVGElement(prc, 'process');
 }
 
 function select(evt) {
-	activeElement = evt.currentTarget;
+	if (evt.currentTarget !== activeElement) {
+		if (activeElement == null) {
+			activeElement = evt.currentTarget;
+		}
+		else {
+			deselect();
+			activeElement = evt.currentTarget;
+		}
+	}
 	if (activeElement) {
 		activeElement.firstChild.setAttributeNS(null, 'fill', 'whiteSmoke');
-		activeElement.setAttributeNS(null, 'onmousemove', 'dragging(evt)');
-		currentX = evt.clientX;
-		currentY = evt.clientY;
-		currentMatrix = activeElement.getAttributeNS(null, 'transform').slice(7, -1).split(' ');
-		for (var i = 0; i < currentMatrix.length; i++) { currentMatrix[i] = parseFloat(currentMatrix[i]); }
+		activeElement.setAttributeNS(null, 'onmousedown', 'pick(evt)');
 	}
 	else {
 		alert('Error: Element is not selected');
 	}
+}
+function deselect(evt) {
+	activeElement.firstChild.setAttributeNS(null, 'fill', 'white');
+	activeElement.setAttributeNS(null, 'onmousedown', null);
+	activeElement.setAttributeNS(null, 'onmousemove', null);
+	activeElement = null;
+}
+function pick(evt) {
+	activeElement.setAttributeNS(null, 'onmousemove', 'dragging(evt)');
+	currentX = evt.clientX;
+	currentY = evt.clientY;
+	currentMatrix = activeElement.getAttributeNS(null, 'transform').slice(7, -1).split(' ');
+	for (var i = 0; i < currentMatrix.length; i++) { currentMatrix[i] = parseFloat(currentMatrix[i]); }
 }
 function dragging(evt) {
 	activeElement.setAttributeNS(null, 'onmouseup', 'drop(evt)');
@@ -131,108 +148,5 @@ function dragging(evt) {
 	currentY = evt.clientY;
 }
 function drop(evt) {
-	activeElement.firstChild.setAttributeNS(null, 'fill', 'white');
-	activeElement.setAttributeNS(null, 'onmousemove', null);
+	return deselect()
 }
-
-
-
-/*
-function randomFromTo(from, to) {
-	return Math.floor(Math.random() * (to - from + 1) + from);
-}
-function init(svg) {
-	/*Initial actions to be done
-	 * right after the svg canvas is created
-	var diagram = svg.group("sd");
-	activeDiagram = "sd";
-}
-
-$(document).ready(function() {
-//	$(".canvas").svg({onLoad: init});
-});
-
-var objId = 0;
-function addObject() {
-	alert("I see you");
-	objId++;
-	var svg = $(".canvas").svg("get");
-	var obj = svg.group($("#" + activeDiagram), "obj" + objId, {transform: 'matrix(1 0 0 1 0 0)'});
-	alert(obj);
-	var xpos = randomFromTo(90, 1150);
-	var ypos = randomFromTo(5, 420);
-	svg.rect(obj, xpos, ypos, 110, 70, {fill: 'white', stroke: 'limeGreen', strokeWidth: '2'});
-	svg.text(obj, xpos + 26, ypos + 42, "Object " + objId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
-	svg.image(obj, xpos + 100, ypos + 60, 9, 9, 'img/gripsmall-se.png');
-	
-	//Binding methods to object
-	$('#obj' + objId)
-		.mousedown(draggingStart)
-		.mouseover(cursorDragging);
-	
-	//Bind methods to resize icon
-	$('#obj' + objId + ' image')
-		.mousedown(resizingStart)
-		.mouseover(cursorResizing);
-	$('#obj' + objId + ' text').editable("", {
-		event: 'dblclick'
-	})
-}
-
-var prcId = 0;
-function addProcess() {
-	prcId++;
-	var svg = $(".canvas").svg("get");
-	var prc = svg.group($("#" + activeDiagram), "prc" + prcId, {transform: 'matrix(1 0 0 1 0 0)'});
-	var xpos = randomFromTo(90, 1150);
-	var ypos = randomFromTo(20, 420);
-	svg.ellipse(prc, xpos, ypos, 60, 40, {fill: 'white', stroke: 'RoyalBlue', strokeWidth: '2'});
-	svg.text(prc, xpos - 33, ypos + 6, "Process "+prcId, {fontFamily: 'Helvetica', fontWeight: 'bold', fontSize: '15'});
-	
-	//Binding methods to process
-	$('#prc' + prcId)
-		.mousedown(draggingStart)
-		.mouseover(cursorDragging);
-}
-
-var currentElement = null;
-var currentX = 0;
-var currentY = 0;
-var currentMatrix = 0;
-function draggingStart(evt) {
-	$(this).bind('mousemove', dragging);								//Bind method to enable dragging
-	$('#' + this.id + ' rect').attr('fill', 'whiteSmoke');				//Change rect bg to represent activity
-	$('#' + this.id + ' ellipse').attr('fill', 'whiteSmoke');			//Change rect bg to represent activity
-	currentX = evt.pageX;												//Prepare for dragging
-	currentY = evt.pageY;
-	currentMatrix = $(this).attr('transform').slice(7, -1).split(' ');
-	for (var i = 0; i < currentMatrix.length; i++)	{ currentMatrix[i] = parseFloat(currentMatrix[i]); }
-}
-function dragging(evt) {
-	$(this).bind('mouseup', draggingStop);
-	dx = evt.pageX - currentX;
-	dy = evt.pageY - currentY;
-	currentMatrix[4] += dx;
-	currentMatrix[5] += dy;
-	var newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-	$(this).attr('transform', newMatrix);
-	currentX = evt.pageX;
-	currentY = evt.pageY;
-}
-function draggingStop(evt) {
-	$(this).unbind('mousemove');
-	$('#' + this.id + ' rect').attr('fill', 'white');
-	$('#' + this.id + ' ellipse').attr('fill', 'white');	
-}
-function resizingStart() {
-	var g  = $(this).parent();
-	alert(g);
-	alert(g.x.animVal.value);
-}
-function cursorDragging() {
-	$(this).css('cursor', 'move');
-}
-function cursorResizing() {
-	$(this).css('cursor', 'se-resize');
-}
-*/
