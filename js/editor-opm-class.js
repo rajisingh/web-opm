df9853d97f1bea474a792a5231cec78517ce9284
rockyray85 <rockyray85@localhost>
2012-04-28 15:47:02 +0300

    updated OPMModel class. indentation changed to double-space added getParticipantList method

--- js/editor-opm-class.js
+++ js/editor-opm-class.js
@@ -6,37 +6,91 @@
  * 	File context description:
  * 	File contains classes description used for OPM
  * 
- *  Author: Sergey N. Bolshchikov
+ *  Author: Rochai Ben-Mordechai & Sameer Makladeh
  * */
+<script>
 
-function OPMModel() {
-	var modelID;
-	var name;
-	var type;
-	var creatorID;
-	var participants[];
-	var sd[];
-	var lastUpdateDate;
-	var creationDate;
+function OPMModel( modelIDVal , creatorIDVal ) {
+  if ( ( typeof modelIDVal !== int ) && ( typeof creatorIDVal !== int ) ){
+    throw "invalid input type. check modelIDVal and creatorIDVal";
+  }
+  
+  var modelID;
+  var name;
+  var type;
+  var creatorID;
+  var participants;
+  var sd;
+  var lastUpdateDate;
+  var creationDate;
+  
+  this.modelID = modelIDVal;
+  this.creatorID = creatorIDVal;
+
+  function getID(){
+    return this.modelID;
+  }
 	
-	function construct( modelID , creatorID ){
-		this.modelID = modelID;
-		this.creatorID = creatorID;
-	}
+  function share( participants[] ){
+    if ( participants === null ){
+      participants = new Array();
+    }
+    this.participants.push(participants[]);
+    return;
+  }
+  
+  function getParticipantList(){
+    if ( this.participants === null ){
+      return "no participating users";
+    }
+    return this.participatns;
+  }
+  
+  function unShare( p ){
+    var x = participants.indexOf( p );
+    if ( x === -1 ){
+      throw "cannot find collaborator ID. please try again.";
+    }
+      var temp = participants[ ( participants.length() ) - 1 ];
+      participants[ ( participants.length() ) - 1 ] = participants [ x ];
+      participants[ x ] = temp;
+      var garbage = participants.pop();
+  }
 	
-	function getID(){
-		return this.modelID;
-	}
+  function getName(){
+	  
+  }
 	
-	function share( participants[] ){
-		this.participants.push(participants[]);
-	}
+  function setName( name ){
+	  
+  }
 	
-	function unShare( participant )
+  function getType(){
+	  
+  }
+	
+  function setType( type ){
+	  
+  }
+	
+  function destructor(){
+    //need procedure for deleting Model from database, including all children.
+    try {
+      if (anything_wrong === true){
+      throw "unable to delete model, please try again";
+      }
+    }
+  }
 }
+
 function OPMDiagram() {}
 function OPMElement() {}
 function OPMEntity() {}
 function OPMThing(){}
 function OPMObject() {}
 function OPMProcess() {}
+function OPMLink() {}
+function OPMProcedural_Link() {}
+function OPMStructural_Link() {}
+
+</script>
